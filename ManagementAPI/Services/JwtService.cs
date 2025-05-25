6acd38abf1ea80c1e28395e2f4f394e7b91567bb
JulioHerculano <julyocesarplay@gmail.com>
2025-05-25 16:11:56 -0300

    Criado o Jwt e implementado

---
+++ ManagementAPI/Services/JwtService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ManagementAPI.Services
+{
+
+
+    public class JwtService(IConfiguration configuration) : IJwtService
+    {
+        private readonly string _secret = Environment.GetEnvironmentVariable("JWT_SECRET") ?? throw new Exception("JWT Secret não configurado.");
+        private readonly string _issuer = Environment.GetEnvironmentVariable("JWT_ISSUER") ?? throw new Exception("JWT Issuer não configurado.");
+        private readonly string _audience = Environment.GetEnvironmentVariable("JWT_AUDIENCE") ?? throw new Exception("JWT Audience não configurado.");
+        private readonly double _expirationMinutes = double.Parse(Environment.GetEnvironmentVariable("JWT_EXPIRE") ?? "30");
+
+        public IConfiguration Configuration { get; } = configuration;
+
+        public string GerarToken(IEnumerable<Claim> claims)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secret));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _issuer,
+                audience: _audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(_expirationMinutes),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+
+}
+
